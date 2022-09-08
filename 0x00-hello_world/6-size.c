bcca843766c3bf5a0a0dd4964f0b629694d8cf2e
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-08 15:11:15 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,9 +18,9 @@ int main(void)
 	char a;
 
 	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %zu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %zu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %zu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %zu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
