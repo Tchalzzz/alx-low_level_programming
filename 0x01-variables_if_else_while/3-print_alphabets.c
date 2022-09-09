71593d36234e3acf6f4fbe8578e529a70f18bf4a
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-09 09:42:04 -0700

    Prints lower and upper letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints letters a-z and A-z
+ *
+ * Return: 0 -Successful
+ *
+ */
+int main(void)
+{
+	/* Declare variables */
+	int ch;
+	/* print lowercase letters */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	/* print uppercase letters */
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar (ch);
+	}
+	return (0);
+
+}
