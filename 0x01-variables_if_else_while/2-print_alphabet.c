170bfc707b6a5a5d6e32572727ed72cd362acb79
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-09 09:05:28 -0700

    a to z using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints characters a to z on the sreen
+ *
+ * Description: Prints letters a-z
+ *
+ * Return: 0 - Success
+ */
+int main(void)
+{
+	/* variable declaration */
+	int ch;
+	/* Loop from a to z and print each cahracter followed by a new line */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+		putchar ("\n");
+	}
+	return (0);
+}
