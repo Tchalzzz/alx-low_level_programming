dc8f24f460ed4ca6fcfbc6cd8c3c50a4e006d285
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-13 04:38:54 -0700

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - A program that prints _putchar
+ *
+ * Return: 0 - Success
+ */
+int main(void)
+{
+	/* variable dclarations */
+	char var[] = "_putchar";
+	int i;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (i = 0; i < lenVar - 1; i++)
+	{
+		_putchar([var]);
+	}
+		_putchar('\n');
+	return (0);
+}
