a015d9eb8afb69d8c23597c0978620148d717058
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-15 10:18:46 -0700

    checks for upper casecharacter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ * Prototype: int _isupper(int c)
+ * @c: ASCII character
+ * Return: 1 if c is uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
