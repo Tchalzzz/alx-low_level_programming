aa07949faca9c9fb07757fce6beedb59f3241f2d
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-21 13:27:10 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ char *leet(char *s)
 	int count = 0, i;
 	int lower_case[] = {97, 101, 111, 116, 108};
 	int upper_case[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49}
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count ) != '\0')
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
