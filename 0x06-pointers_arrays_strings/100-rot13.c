1a9d311711e07670973c914fbf1c5e3ab050b5ef
Effiong Ekpe <effiongekpe02@gmail.com>
2022-09-22 05:12:05 -0700

    Pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,24 +3,24 @@
 /**
  * rot13 - encodes a string using rot13
  * @s: input string
- * Return: The pointer to dest
+ * Return: the pointer to dest
  */
-
 char *rot13(char *s)
 {
 	int count = 0, i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) = '\0')
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + count) == alphabet[i]
+			if (*(s + count) == alphabet[i])
+			{
 				*(s + count) = rot13[i];
 				break;
+			}
 		}
-
 		count++;
 	}
 
