86621abdfb2251b33e6bc130832254522770a783
Effiong Ekpe <effiongekpe02@gmail.com>
2022-10-10 11:47:19 -0700

    Structures and Typeded

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,14 +3,21 @@
 #include <stdlib.h>
 
 /**
- * print_dog - Prints dog info.
- * @dog: struct wit labe dog
+ * print_dog - Prints attributes of the structure info.
+ * @d: new instance
  */
 
 void print_dog(struct dog *d)
 {
-	id (d != NULL)
+	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name != NULL ? d->name: "nil)", (*d).age, (*d).owner != NULL ? d->owner : "(nil)");
+		if ((*d).name == NULL && (*d).owner == NULL)
+		{
+			printf("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
+		}
+		else
+		{
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		}
 	}
 }
