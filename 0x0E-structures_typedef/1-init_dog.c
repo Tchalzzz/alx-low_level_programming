a5e7e7db6516def8206d7fe657cf92e6c8c34c7b
Effiong Ekpe <effiongekpe02@gmail.com>
2022-10-10 11:06:26 -0700

    Structures and Typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: string for owner
  */
 
-void init_dog(struct dog *d, char *name, float age, char owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
