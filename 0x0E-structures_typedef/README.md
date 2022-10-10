4f662b7562f95b8c37c3c08fe3cfc7ba9af2fbcb
Effiong Ekpe <effiongekpe02@gmail.com>
2022-10-10 00:22:32 -0700

    Structures and typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project contains tasks pertaining to data structures in C.struct and typedef tasks are populated here.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - defines a structure called dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: dog defines a data structure of type
+ * struct wih the label dog. It defines 3 members that are
+ * attributes of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H_
