1a9cc5135be26e2c55e1e4da5338579c04124ec2
Effiong Ekpe <effiongekpe02@gmail.com>
2022-10-13 00:19:21 -0700

    Function Pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,34 +4,35 @@
 
 /**
  * main - Entry point
- *
+ * @argc: The number of arguments suplied
+ * @argv: Array of pointers to the argument
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__ ((__unused__)) argc, char *argv[])
 {
-	int a = 0, b = 0, result = 0;
-	char s;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	s = argv[2][0];
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (s != '+' && s != '-' && s != '*' && s != '/' && s != '%')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	result = (get_op_func(argv[2]))(a, b);
-	printf("%d\n", result);
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
